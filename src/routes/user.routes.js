36c37e1fb389a479e66a5f9df8ec286f30bd9c37
Anubhav Baranwal <anubhavbaranwal08@gmail.com>
2023-12-30 19:10:14 +0530

    add user route for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,6 +5,12 @@ import {
   LogOut,
   RefessAccessToken,
   getChannelandSubscriber,
+  changePassword,
+  currentUser,
+  updateUserDetails,
+  updateUserAvatar,
+  updateUsercoverImage,
+  getWatchHistory,
 } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { VerifyJWT } from "../middlewares/auth.middleware.js";
@@ -28,13 +34,24 @@ router.route("/login").post(LoginUser);
 //secured Routes
 router.route("/logout").post(VerifyJWT, LogOut);
 router.route("/refresh-token").post(RefessAccessToken);
+router.route("/change-password").post(VerifyJWT, changePassword);
+router.route("/current-user").get(VerifyJWT, currentUser);
+router.route("/update-userdetail").patch(VerifyJWT, updateUserDetails);
+router
+  .route("/update-avatar")
+  .patch(VerifyJWT, upload.single("/avatar"), updateUserAvatar);
+router
+  .route("/update-coverImage")
+  .patch(VerifyJWT, upload.single("/coverimage"), updateUsercoverImage);
+router.route("/getchannel/:username").post(VerifyJWT, getChannelandSubscriber);
+router.route("/watchhistory").get(VerifyJWT,getWatchHistory)
 
 //for video
-router.route("/upload").post(
-  upload.fields([
-    { name: "video", maxCount: 1 },
-    { name: "thumbnail", maxCount: 1 },
-  ]),
-  uploadVideo
-);
+// router.route("/upload").post(
+//   upload.fields([
+//     { name: "video", maxCount: 1 },
+//     { name: "thumbnail", maxCount: 1 },
+//   ]),
+//   uploadVideo
+// );
 export default router;
