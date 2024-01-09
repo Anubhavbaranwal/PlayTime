fe44b147a3e33997d589b946958fbd9ec1dfac4b
Anubhav Baranwal <anubhavbaranwal08@gmail.com>
2024-01-09 12:11:52 +0530

    addcomment controller

--- src/controllers/Comment.controller.js
+++ src/controllers/Comment.controller.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 import { Comment } from "../models/comment.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Apiresponse } from "../utils/Apiresponse.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
+import { asynchandling } from "../utils/asyncHandler.js";
 
-const getVideoComments = asyncHandler(async (req, res) => {
+const getVideoComments = asynchandling(async (req, res) => {
   //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
@@ -22,21 +22,39 @@ const getVideoComments = asyncHandler(async (req, res) => {
     .skip((page - 1) * limit)
     .limit(limit)
     .sort({ createdAt: -1 });
- 
+
   return res
     .status(200)
     .json(new Apiresponse(201, comments, "omments fetched Successfully"));
 });
 
-const addComment = asyncHandler(async (req, res) => {
+const addComment = asynchandling(async (req, res) => {
   // TODO: add a comment to a video
+  const { videoId } = req.params;
+  if (!videoId) {
+    throw new ApiError(411, "VideoId is Not Valid");
+  }
+  const { content } = req.body;
+  if (!content) {
+    throw new ApiError(400, "Please provide the comment");
+  }
+
+  const commentdone = new Comment.create({
+    user: req.User._id,
+    video: videoId,
+    content,
+  });
+
+  return req
+    .status(200)
+    .json(new Apiresponse(200, commentdone, "comment done successfully"));
 });
 
-const updateComment = asyncHandler(async (req, res) => {
+const updateComment = asynchandling(async (req, res) => {
   // TODO: update a comment
 });
 
-const deleteComment = asyncHandler(async (req, res) => {
+const deleteComment = asynchandling(async (req, res) => {
   // TODO: delete a comment
 });
 
